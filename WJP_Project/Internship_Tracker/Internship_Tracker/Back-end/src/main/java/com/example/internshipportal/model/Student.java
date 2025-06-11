cc47fbc0f77b8f06c43c966244a9433976358af9
Shital-Kawthe <shitalkawthe1994@gmail.com>
2025-06-11 19:58:19 +0530

    created a web app using React.js Spring Boot abd MySQL.

---
+++ WJP_Project/Internship_Tracker/Internship_Tracker/Back-end/src/main/java/com/example/internshipportal/model/Student.java
@@ -0,0 +1,18 @@
+package com.example.internshipportal.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student extends User {
+    private String course;
+    private String branch;
+    private String year;
+    private String rollNumber;
+    private String college;
+    private boolean active;
+
+	}
